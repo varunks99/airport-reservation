7476462d19092b0072d9ad4c5b14d7afa251b4d3
varunks99 <varunshiri99@gmail.com>
2020-03-22 19:40:34 +0530

    Landing page

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const path = require('path')
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const uri = "mongodb+srv://user:iamauser@airport-wonyq.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+let app = express();
 
-const {Airport, Airline, Passenger, Flight, Ticket} = require('./models/index')
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
-// let newairport = new Airport({
-//   name: 'Belagavi Airpot',
-//   code: 'IXG',
-//   location: 'Belagavi'
-// });
-//
-// newairport.save((err, doc) => {
-//   console.log(doc);
-// })
+const {Airport, Airline, Passenger, Flight, Ticket} = require('./models/index');
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+})
