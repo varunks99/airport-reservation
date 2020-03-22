4ea958f945a97521014e1ca3a8cb3466fa4afd49
varunks99 <varunshiri99@gmail.com>
2020-03-22 14:00:22 +0530

    Mongoose code

--- server.js
+++ server.js
@@ -1,27 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://user:iamauser@airport-wonyq.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 
-async function main() {
-  async function listDatabases() {
-    let databasesList = await client.db().admin().listDatabases()
-    databasesList.databases.forEach(db=> {
-      console.log(db.name);
-    })
-  }
+const {Airport, Airline, Passenger, Flight, Ticket} = require('./models/index')
 
-  const uri = "mongodb+srv://user:iamauser@airport-wonyq.mongodb.net/test?retryWrites=true&w=majority";
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
-  try {
-    await client.connect()
-    await listDatabases(client)
-  }
-  catch  (e){
-    console.error(e);
-  }
-  finally {
-    await client.close()
-  }
-}
-
-main().catch(console.error)
+// let newairport = new Airport({
+//   name: 'Belagavi Airpot',
+//   code: 'IXG',
+//   location: 'Belagavi'
+// });
+//
+// newairport.save((err, doc) => {
+//   console.log(doc);
+// })
